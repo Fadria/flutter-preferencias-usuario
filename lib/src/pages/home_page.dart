5da11e9344a26ca4fae49756b48f5123cbe64ba0
Federico Adria <fadriacarrasco@gmail.com>
2021-10-11 13:41:25 +0200

    Drawers - Crear un menú lateral

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_usuario/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -12,6 +13,7 @@ class HomePage extends StatelessWidget {
       (
         title: Text("Preferencias de Usuario"),
       ),
+      drawer: _crearMenu(context),
       body: Column
       (
         mainAxisAlignment: MainAxisAlignment.center,
@@ -27,4 +29,64 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Drawer _crearMenu(BuildContext context) 
+{
+  return Drawer
+  (
+    child: ListView
+    (
+      padding: EdgeInsets.zero,
+      children: <Widget>
+      [
+        DrawerHeader
+        (
+          child: Container
+          (
+            
+          ),
+          decoration: BoxDecoration
+          (
+            image: DecorationImage
+            (
+              image: AssetImage('assets/menu-img.jpg'), fit: BoxFit.cover
+            )
+          ),
+        ),
+
+        ListTile
+        (
+          leading: Icon(Icons.house, color: Colors.blue,),
+          title: Text("Home"),
+          onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+        ),
+
+        ListTile
+        (
+          leading: Icon(Icons.party_mode, color: Colors.blue,),
+          title: Text("Party Mode"),
+          onTap: (){},
+        ),
+
+        ListTile
+        (
+          leading: Icon(Icons.people, color: Colors.blue,),
+          title: Text("People"),
+          onTap: (){},
+        ),
+
+        ListTile
+        (
+          leading: Icon(Icons.settings, color: Colors.blue,),
+          title: Text("Settings"),
+          onTap: ()
+          {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+          },
+        ),
+      ],
+    ),
+  );
 }
